5c983b3bc73a038a528cb59af4642eb6389751e8
snehasamuel <snehasamuel297@gmail.com>
2022-05-26 11:17:46 +0530

    Header  branch completed

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+<Link class="navbar-brand" to="/">STUDENT APP</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+<Link class="nav-link" to="/">Add Sudent</Link>
+      </li>
+      <li class="nav-item">
+      <Link class="nav-link" to="/view">View Student</Link>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Header
