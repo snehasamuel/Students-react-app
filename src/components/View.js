09c2c139f3c02b6419ba99f759a5ba558c738bb0
snehasamuel <snehasamuel297@gmail.com>
2022-05-26 11:46:01 +0530

    View  branch completed

--- src/components/View.js
+++ src/components/View.js
@@ -12,11 +12,12 @@ const View = () => {
   return (
     <div>
         <Header/>
-
+<br>
+</br>
 <div className='container'>
     <div className='row'>
         <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-        <table class="table">
+        <table class="table table-primary">
   <thead>
     <tr>
       <th scope="col">Name</th>
