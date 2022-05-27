9dd2ad3c63cc99a40ecbb6bf1cd0fe151456f2b7
snehasamuel <snehasamuel297@gmail.com>
2022-05-27 11:57:00 +0530

    search branch

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,11 @@
-<div className="container">
+import React from 'react'
+import Header from './Header'
+
+const Search = () => {
+  return (
+    <div>
+       <Header/>
+        <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
@@ -13,3 +20,8 @@
         </div>
     </div>
 </div>
+    </div>
+  )
+}
+
+export default Search
